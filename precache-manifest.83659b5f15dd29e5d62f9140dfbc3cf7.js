02418f6c125c6de4251fe1a25117409432a7f235
unknown <hungly1802@gmail.com>
2019-09-25 10:22:26 +0700

    Updates

---
+++ precache-manifest.83659b5f15dd29e5d62f9140dfbc3cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d66ab3e11c5c15f589a093973b6e74",
+    "url": "/test2/index.html"
+  },
+  {
+    "revision": "d85da61755312c8fdeac",
+    "url": "/test2/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "93b8f9b444d1a857a797",
+    "url": "/test2/static/js/2.f88a4253.chunk.js"
+  },
+  {
+    "revision": "d85da61755312c8fdeac",
+    "url": "/test2/static/js/main.e064cea7.chunk.js"
+  },
+  {
+    "revision": "0c1614e2d6af7376a394",
+    "url": "/test2/static/js/runtime-main.36c3d2f9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test2/static/media/logo.25bf045c.svg"
+  }
+]);
